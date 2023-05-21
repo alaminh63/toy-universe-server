e4d3116326ca2715da1481d044667f5368623fc2
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-05-21 20:52:26 +0600

    error sloved

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+   
     client.connect();
 
     const toyCollection = client.db("toyUniverse").collection("toyInfo");
