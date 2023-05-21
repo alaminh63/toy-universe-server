c89f273e89c3fa3e9eed6d54e90b160986db44b9
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-05-21 21:42:14 +0600

    mongo db new data collection added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdzlhd7.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
